21ce5c8ed1129d6bb582d430a883173e46e57ada
mahmoudomry <mahmoudomry@gmail.com>
2022-10-10 19:04:47 +0300

    Add project files.

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtTop.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/ServicesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtTop.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
