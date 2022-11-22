1c9ac83ab49cd0018a72aa6a19cc0545e3541147
mahmoudomry <mahmoudomry@gmail.com>
2022-11-23 00:06:22 +0200

    commit all

---
+++ Areas/Identity/Data/ArtTopContext.cs
@@ -0,0 +1,33 @@
+﻿using ArtTop.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ArtTop.Data;
+
+public class ArtTopContext : IdentityDbContext<ApplicationUser>
+{
+    public ArtTopContext(DbContextOptions<ArtTopContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.Entity<Slider>().ToTable("Slider");
+    }
+    public DbSet<Slider> Sliders { get; set; }
+}
+internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(x => x.FullName).HasMaxLength(250);
+
+    }
+}
