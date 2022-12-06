f43c69f3c2cdcd444f2208be74b8eba51d72d5ae
mahmoudomry <mahmoudomry@gmail.com>
2022-12-06 13:56:41 +0200

    commit all

--- wwwroot/lib/ckeditor/config.js
+++ wwwroot/lib/ckeditor/config.js
@@ -7,32 +7,38 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
-
+	config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,notification,button,toolbar,clipboard,panelbutton,panel,floatpanel,colorbutton,colordialog,xml,ajax,templates,menu,contextmenu,copyformatting,div,editorplaceholder,resize,elementspath,enterkey,entities,exportpdf,popup,filetools,filebrowser,find,floatingspace,listblock,richcombo,font,fakeobjects,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastetools,pastefromgdocs,pastefromlibreoffice,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,tableselection,undo,lineutils,widgetselection,widget,notificationaggregator,uploadwidget,uploadimage';
+	config.skin = 'moono-lisa';
 	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
+	//config.toolbarGroups = [
+	//	{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+	//	{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+	//	{ name: 'links' },
+	//	{ name: 'insert' },
+	//	{ name: 'forms' },
+	//	{ name: 'tools' },
+	//	{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+	//	{ name: 'others' },
+	//	'/',
+	//	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+	//	{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+	//	{ name: 'styles' },
+	//	{ name: 'colors' },
+	//	{ name: 'justify' },
+	//	{ name: 'colorbutton' },
+	//	{ name: 'colordialog' },
+	//	{ name: 'about' }
+	//];
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	//config.removeButtons = 'Underline,Subscript,Superscript';
 
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+	//// Set the most common block elements.
+	//config.format_tags = 'p;h1;h2;h3;pre';
 
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+	//// Simplify the dialog windows.
+	//config.removeDialogTabs = 'image:advanced;link:advanced';
+	//config.colorButton_colors = 'CF5D4E,454545,FFF,DDD,CCEAEE,66AB16';
+	//config.colorButton_enableAutomatic = false;
 };
