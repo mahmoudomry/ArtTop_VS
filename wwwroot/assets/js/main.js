71a21424c493785f9e50f82988ce008b94e659a3
Mohammed Alghazali <dev.mghazali@gmail.com>
2022-12-13 06:41:07 +0200

    3 level menu

--- wwwroot/assets/js/main.js
+++ wwwroot/assets/js/main.js
@@ -1,5 +1,3 @@
-
-
 inlineSVG.init(
   {
     svgSelector: "img.svg-inline", // the class attached to all images that should be inlined
@@ -18,4 +16,52 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("myOverlay").style.display = "none";
 }
+let dropdowns = document.querySelectorAll(".dropdown-toggle");
+dropdowns.forEach((dd) => {
+  dd.addEventListener("click", function (e) {
+    var el = this.nextElementSibling;
+    el.style.display = el.style.display === "block" ? "none" : "block";
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  /////// Prevent closing from click inside dropdown
+  document.querySelectorAll(".dropdown-menu").forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.stopPropagation();
+    });
+  });
+
+  // make it as accordion for smaller screens
+  if (window.innerWidth < 992) {
+    // close all inner dropdowns when parent is closed
+    document
+      .querySelectorAll(".navbar .dropdown")
+      .forEach(function (everydropdown) {
+        everydropdown.addEventListener("hidden.bs.dropdown", function () {
+          // after dropdown is hidden, then find all submenus
+          this.querySelectorAll(".submenu").forEach(function (everysubmenu) {
+            // hide every submenu as well
+            everysubmenu.style.display = "none";
+          });
+        });
+      });
+
+    document.querySelectorAll(".dropdown-menu a").forEach(function (element) {
+      element.addEventListener("click", function (e) {
+        let nextEl = this.nextElementSibling;
+        if (nextEl && nextEl.classList.contains("submenu")) {
+          // prevent opening link if link needs to open dropdown
+          e.preventDefault();
+          console.log(nextEl);
+          if (nextEl.style.display == "block") {
+            nextEl.style.display = "none";
+          } else {
+            nextEl.style.display = "block";
+          }
+        }
+      });
+    });
+  }
+  // end if innerWidth
+});
 AOS.init();
